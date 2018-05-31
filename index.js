82e154581179847330668998772365d5bb28a55a
Abegg <Christian.Abegg@zuehlke.com>
2018-05-31 17:29:21 +0200

    implementeed hooks

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 module.exports = {
     // Map of hooks
-    hooks: {},
-
-    // Map of new blocks
-    blocks: {
-        onlineresources: {
-            process: function(block) {
-                return "Hello "+block.body+", How are you?";
+    hooks: {
+        "page:before": function(page) {
+            if (this.output.name === 'website') {
+                if (page.content.includes('.resources.md" %}')) {
+                    // remove this line
+                    const regex = /{% include "\.\/.*\.resources\.md" %}/
+                    page.content = page.content.replace(regex, "");
+                    
+                    // add reference link
+                    let relativeUrl = page.path.replace('.md', '.html').replace("\\", "/");
+                    let basePath = this.config.values.pluginsConfig.onlineresources.url;
+                                        
+                    let referencesLink = `[Additional resources online](${basePath}${relativeUrl})`;
+                    page.content = page.content + referencesLink;
+                }
             }
+            return page;
         }
     },
+
+    // Map of new blocks
+    blocks: {},
 
     // Map of new filters
     filters: {}
